a7ceaf6f7dcd2ce54b20169bebd4c451be47caee
Santiago Estrada Bernal <santiago.estrada6@eia.edu.co>
2026-03-13 22:37:32 -0500

    Add launch file for Stanley race node with configuration for simulation and RViz

--- delta_bringup/launch/gz_spawn.launch.py
+++ delta_bringup/launch/gz_spawn.launch.py
@@ -13,7 +13,7 @@ import xacro
 ack = False
 maze = ["empty_world.sdf", "j_maze.sdf", "melgui_maze.sdf", "DemoRaceTrack.sdf", "RaceTrack.sdf", "RaceTrackObs.sdf" , "walls_world2.sdf"]
 xacro_model = "ackerman_robot.urdf.xacro" if ack else "robot.urdf.xacro"
-gz_world = maze[-1] # Change the index to select a different world
+gz_world = maze[4] # Change the index to select a different world
 
 def generate_launch_description():
     gazebo_pkg_name = "delta_gazebo"
@@ -70,8 +70,8 @@ def generate_launch_description():
         arguments=[
             "-name", "diffbot",
             "-topic", "robot_description",
-            "-x", "0.0", "-y", "0.0", "-z", "1.0", "-R", "0.0", "-P", "0.0", "-Y", "0.0"
-            # "-x", "-18.44", "-y", "4.29", "-z", "3.0", "-R", "0.0", "-P", "0.0", "-Y", "1.57"
+            # "-x", "0.0", "-y", "0.0", "-z", "1.0", "-R", "0.0", "-P", "0.0", "-Y", "0.0"
+            "-x", "-18.44", "-y", "4.29", "-z", "3.0", "-R", "0.0", "-P", "0.0", "-Y", "1.57"
         ],
     )
 
--- delta_path_planner/launch/delta_single_path.launch.py
+++ delta_path_planner/launch/delta_single_path.launch.py
@@ -50,6 +50,7 @@ def generate_launch_description():
 						"use_sim_time": use_sim_time,
 						"waypoints": use_waypoints,
 						"topics.path_topic": "/planned_path",
+						"geometry.inflate_radius": 0.65,
 					}
 				],
 				condition=IfCondition(EqualsSubstitution(planner_index, str(idx))),
@@ -64,7 +65,9 @@ def generate_launch_description():
 		parameters=[{"use_sim_time": use_sim_time,
 			   "use_start": True,
 			   "manual": True,
-			   "closed_loop": True}],
+			   "closed_loop": True,
+			   "num_points": 5,
+			   "waypoints_file": "/home/santy-estrada/mrad_ws_2601_delta/src/delta_path_planner/waypoints_json/sample_waypoints.json"}],
 		condition=IfCondition(use_waypoints),
 	)
 
@@ -113,7 +116,7 @@ def generate_launch_description():
 			),
 			DeclareLaunchArgument(
 				"use_waypoints",
-				default_value="true",
+				default_value="false",
 				description="If true planner listens to /waypoints_topic and launches waypoints_node.",
 			),
 			gz_spawn_launch,
